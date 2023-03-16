46d185b3870f31db7d5cd34b850280d92bd58ea4
Syed Raza <syed.raza011@gmail.com>
2023-03-16 16:28:33 -0400

    updates on nav bar and links

--- .next/static/chunks/pages/about-2288c726a6d2a18a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[521],{512:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about",function(){return t(843)}])},843:function(n,u,t){"use strict";t.r(u);var _=t(5893);t(7294),t(1664);let e=()=>(0,_.jsx)("h1",{children:"About page"});u.default=e}},function(n){n.O(0,[664,774,888,179],function(){return n(n.s=512)}),_N_E=n.O()}]);
---
+++ components/NavigationBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Link from "next/link";
+
+
+export default function NavigationBar() {
+  return (
+    <>
+      <div className="nav-class">
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/contact">Contact</Link>
+        <Link href="/flights">Flight</Link>
+        <Link href="/users">Users</Link>
+        <Link href="/checkout">checkout</Link>
+      </div>
+    </>
+  );
+}
