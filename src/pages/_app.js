72203bdf4291d261002e1e8871e532500986ec11
Ron <ronnieangeles@Ron.attlocal.net>
2023-03-16 17:46:37 -0500

    navandfooter

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import React from "react";
 import NavigationBar from "/components/NavigationBar";
+import Footer from "/components/Footer";
 import "../styles/globals.css";
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
@@ -16,8 +17,8 @@ function MyApp({ Component, pageProps }) {
 
       <NavigationBar />
       <Component {...pageProps} />
-
-      <footer className={styles.footer}>
+      <Footer />
+      {/* <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
@@ -28,7 +29,7 @@ function MyApp({ Component, pageProps }) {
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
-      </footer>
+      </footer> */}
     </>
   );
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,31 @@
 import react from "react";
 import Link from "next/link";
 const About = () => {
-  return <h1>About page</h1>;
-
-  <Link href="/">Home page</Link>
+  return (
+    <>
+      <h1>About page</h1>
+      <div>
+        <p>
+          Welcome to Starbound, the premier space tourism company for those
+          seeking a truly out-of-this-world experience. Our mission is to make
+          space travel accessible and safe for everyone, so that you can
+          experience the thrill of exploring the cosmos for yourself. At
+          Starbound, we offer a range of exciting and unforgettable space travel
+          experiences, from short suborbital flights to longer stays aboard the
+          International Space Station. Our state-of-the-art spacecraft are
+          designed for maximum comfort and safety, so you can relax and enjoy
+          the ride while our experienced crew takes care of everything else.
+          Whether you’re a space enthusiast looking to fulfill a lifelong dream,
+          an adventurer seeking a new challenge, or simply looking to experience
+          the awe-inspiring beauty of the universe, Galactic Adventures has a
+          space travel package that’s perfect for you. Our team of experts will
+          guide you through every step of the process, from pre-flight training
+          to your actual space flight, to ensure that you have a safe,
+          enjoyable, and truly unforgettable experience. So why wait? Contact us
+          today to start planning your journey to the stars with Starbound!
+        </p>
+      </div>
+    </>
+  );
 };
 export default About;
