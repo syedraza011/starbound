f9f5ccfc34073cec31200ff9b482c7e56ec7bbe8
Syed Raza <syed.raza011@gmail.com>
2023-03-16 16:55:10 -0400

    updates on nav bar and links

--- components/NavigationBar.js
+++ components/NavigationBar.js
@@ -1,15 +1,17 @@
 import React from "react";
 import Link from "next/link";
 
-
 export default function NavigationBar() {
   return (
     <>
       <div className="nav-class">
         <Link href="/">Home</Link>
+
         <Link href="/about">About</Link>
         <Link href="/contact">Contact</Link>
+        <Link href="/flights">Depating Flights</Link>
         <Link href="/flights">Flight</Link>
+        <Link href="/training">Training</Link>
         <Link href="/users">Users</Link>
         <Link href="/checkout">checkout</Link>
       </div>
