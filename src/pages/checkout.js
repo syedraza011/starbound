5f755c2efc34341358aa2af89cd6718c9f50b6ee
Syed Raza <syed.raza011@gmail.com>
2023-03-16 12:54:58 -0400

    added training, Checkout, userRoutes and flight route files

---
+++ src/pages/checkout.js
@@ -0,0 +1,8 @@
+import react from "react";
+import Link from "Next/link";
+const Checkout = () => {
+  return <h1>Checkout page</h1>;
+
+  <Link href="/">Home page</Link>;
+};
+export default Checkout;
