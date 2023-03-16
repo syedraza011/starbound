461eee1962366ec31c05fef126e7c19cdc13c2b4
Ron <ronnieangeles@Ron.attlocal.net>
2023-03-16 16:37:22 -0500

    nav

---
+++ src/pages/flights.js
@@ -0,0 +1,12 @@
+import react from "react";
+import Link from "Next/link";
+const Flights = () => {
+  return (
+    <>
+      <h1>Flightspage</h1>
+
+      <Link href="/">Home page</Link>
+    </>
+  );
+};
+export default Flights;
