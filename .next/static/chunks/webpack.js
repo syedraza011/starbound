2284603957fd1d0da63bd1289062cb07681dd8b2
Ron <ronnieangeles@Ron.attlocal.net>
2023-03-16 17:49:25 -0500

    updated nav and footer

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "33c71c10965d7af4"; }
+/******/ 		__webpack_require__.h = function() { return "f3fa83f2b2ed4aea"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
