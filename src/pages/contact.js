70609f69f67005ade7831231ef8d88aed7247679
Syed Raza <syed.raza011@gmail.com>
2023-03-16 15:44:47 -0400

    updated contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,8 @@
+import react from "react";
+import Link from "Next/link";
+const Contact = () => {
+  return <h1>Contact page</h1>;
+
+  <Link href="/">Home page</Link>;
+};
+export default Contact;
